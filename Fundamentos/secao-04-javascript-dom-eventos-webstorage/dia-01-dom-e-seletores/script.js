f2351c08e354353c4fba6783e293f2b9c0b9af54
Michael <michaellfernandess@gmail.com>
2023-05-13 17:25:45 -0300

    dia 01 exercicios

--- Fundamentos/secao-04-javascript-dom-eventos-webstorage/dia-01-dom-e-seletores/script.js
+++ Fundamentos/secao-04-javascript-dom-eventos-webstorage/dia-01-dom-e-seletores/script.js
@@ -6,21 +6,29 @@ emergencyTask.style.backgroundColor = 'salmon';
 
 
 const noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3');
-for( let index = 0; index < noEmergencyTasksHeaders.length; index += 1){
-noEmergencyTasksHeaders[index].style.backgroundColor = 'black'
+for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+    noEmergencyTasksHeaders[index].style.backgroundColor = 'black'
 };
 
 const corDaLetra = document.querySelector('#header-container');
 corDaLetra.style.color = 'white';
 
 const corDaLetra2 = document.querySelectorAll('#container h3');
-for (let index = 0; index < corDaLetra2.length; index += 1){
+for (let index = 0; index < corDaLetra2.length; index += 1) {
     corDaLetra2[index].style.color = 'white'
-};console.log(corDaLetra2);
-
-const amarelo = document.querySelectorAll('.no-emergency-tasks div');
-for (let index = 0; index < amarelo.length; index += 1){
-    amarelo[index].style.backgroundColor = 'yellow'
 };
 
-console.log(amarelo);
+const amarelo = document.querySelector('.no-emergency-tasks');
+amarelo.style.backgroundColor = 'yellow'
+
+
+const emergencyTask2 = document.querySelectorAll('.emergency-tasks h3');
+for (let index = 0; index < emergencyTask2.length; index += 1) {
+    emergencyTask2[index].style.backgroundColor = 'purple'
+};
+
+const rodape = document.querySelector('#footer-container');
+rodape.style.backgroundColor = 'darkgreen'; 
+
+const letraRodape = document.querySelector('#footer-container div');
+letraRodape.style.color = 'white';
