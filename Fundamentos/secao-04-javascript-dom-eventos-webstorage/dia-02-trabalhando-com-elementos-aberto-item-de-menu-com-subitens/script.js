db2ba04c3a42ca55c5f9004902ea7866ec3dc110
Michael <michaellfernandess@gmail.com>
2023-05-14 00:22:15 -0300

    Exercicios dia 02

--- Fundamentos/secao-04-javascript-dom-eventos-webstorage/dia-02-trabalhando-com-elementos-aberto-item-de-menu-com-subitens/script.js
+++ Fundamentos/secao-04-javascript-dom-eventos-webstorage/dia-02-trabalhando-com-elementos-aberto-item-de-menu-com-subitens/script.js
@@ -2,3 +2,16 @@ const whereAreYou = document.querySelector('#where-are-you');
 
 const parent = whereAreYou.parentElement;
 parent.style.color = 'red';
+
+const firstChildOfChild = whereAreYou.firstElementChild;
+firstChildOfChild.innerText = 'First'
+
+const firstChild = parent.firstElementChild;
+
+const primeiroFilho2 = whereAreYou.previousElementSibling;
+
+const textElement = whereAreYou.nextSibling;
+
+const thirdChild = whereAreYou.nextElementSibling;
+
+const thirdChild2 = parent.lastElementChild.previousElementSibling;
